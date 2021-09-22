e53c9d31507936152e2ee91496c86eaf7a4149f0
Atul Varshneya <atul.varshneya@gmail.com>
2021-09-21 20:51:04 -0700

    Included the samba information, make backups remotely accessible.

--- conf-save/cp_conf_files.sh
+++ conf-save/cp_conf_files.sh
@@ -2,3 +2,5 @@ cp /etc/fstab fstab
 crontab -l > crontab
 cp /etc/systemd/system/sjwebui.service .
 cp /shares/USBDRIVE/SYNCJOBS.conf/etc/* ./etc
+# Save samba conf too
+cp /etc/samba/smb.conf ./smb
