ead23c14b854bdd5b2e77e47e614098ce04e5560
Atul Varshneya <atul.varshneya@gmail.com>
2021-09-22 22:57:36 -0700

    using flask templates

--- sjwebui.py
+++ sjwebui.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-"""basic Flask app - demo of using a variable in a route"""
-
 import os
 import config
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
 
+app = Flask(__name__)
 
 def readreg():
 	regfile = os.environ.get("SJREGISTRY")
@@ -15,62 +14,33 @@ def readreg():
 	reg = config.Config(regfile).read_registry()
 	return reg
 
-def pageheader():
-	titletext = "<h1>SyncJobs</h1>"
-	links = "<p>" + \
-		"<a href='/'>Home</a> &emsp; " + \
-		"<a href='/logfile'>Log File</a> &emsp; " + \
-		"<a href='/jobfile'>Jobs List</a> " + \
-		"</p>"
-	ruler = "<hr>"
-	return titletext + links + ruler
-
 @app.route('/')
-def hello():
+def regfile():
 	reg = readreg()
-	outtext = ""
-	for r in reg.keys():
-		outtext = outtext + \
-			"<div style=\"font-size:16px\">" + \
-			"<pre>" + \
-			r + ":\n\t" + \
-			reg[r] + "\n\n" + "</pre>"
-		outtext = outtext + "</div>"
-	return pageheader() + outtext
+	return render_template('home.html', reg=reg)
 
 @app.route('/jobfile')
 def jobfile():
 	reg = readreg()
 	jobfile = reg["jobsloc"]
 	jobs = config.Config(jobfile).read_entries()
-	outtext = ""
-	for i in jobs:
-		outtext = outtext + \
-			"<p>" +\
-			"<pre>" + \
-			"\nJOB:  " + i[0] + \
-			"\n\tSource      :\t" + i[1] + \
-			"\n\tDestination :\t" + i[2] + \
-			"\n\tFlags       :\t" + i[3] + \
-			"</pre>" + \
-			"</p>"
-	return pageheader() + outtext
+	return render_template('jobs.html', jobs=jobs)
 
 frames = []
-totcnt = []
-frameid = 0
+fmtots = []
+fmlast = 0
 scrframe = 0
 total_credirs, total_cpfiles, total_deldirs, total_delfiles, total_errors = 0,0,0,0,0
 
 @app.route('/logfile')
 def logfile():
-	global frames, totcnt, frameid, scrframe
+	global frames, fmtots, fmlast, scrframe
 	global total_credirs, total_cpfiles, total_deldirs, total_delfiles, total_errors
 
 	reg = readreg()
 	frames = []
-	totcnt = []
-	frameid = 0
+	fmtots = []
+	fmlast = 0
 	scrframe = 0
 	logtext = ""
 	total_credirs, total_cpfiles, total_deldirs, total_delfiles, total_errors = 0,0,0,0,0
@@ -81,8 +51,8 @@ def logfile():
 				break
 			if ln[20:27] == "STARTED":
 				frames.append(logtext)
-				totcnt.append((total_credirs, total_cpfiles, total_deldirs, total_delfiles, total_errors))
-				frameid = frameid + 1
+				fmtots.append((total_credirs, total_cpfiles, total_deldirs, total_delfiles, total_errors))
+				fmlast = fmlast + 1
 				logtext = ""
 				total_credirs, total_cpfiles, total_deldirs, total_delfiles, total_errors = 0,0,0,0,0
 			if ln[20:32] == "Created dirs":
@@ -98,42 +68,19 @@ def logfile():
 				total_errors = total_errors + int(ln[27:])
 			logtext = logtext + ln
 		frames.append(logtext)
-		totcnt.append((total_credirs, total_cpfiles, total_deldirs, total_delfiles, total_errors))
-		scrframe = frameid
-	return pageheader() + framebar() + "<pre>" + frames[scrframe] + "</pre>" + "<hr>" + framebar()
-
-def framebar():
-	global frames, totcnt, frameid, scrframe
-
-	nextframe = "/frame/{:d}".format(scrframe+1)
-	prevframe = "/frame/{:d}".format(scrframe-1)
-	(credirs,cpfiles,deldirs,delfiles,errors) = totcnt[scrframe]
-	fbartext = "<p>"
-	if (scrframe > 0):
-		fbartext = fbartext + "<a href=" + prevframe + ">prev</a>"
-	else:
-		fbartext = fbartext + "prev"
-	fbartext = fbartext + " move to "
-	if (scrframe < frameid):
-		fbartext = fbartext + "<a href=" + nextframe + ">next</a>"
-	else:
-		fbartext = fbartext + "next"
-	fbartext = fbartext + "&emsp;" + \
-		"| &emsp; Copied: Dirs "+str(credirs)+", Files "+str(cpfiles)+ \
-		". &emsp; Deleted: Dirs "+str(deldirs)+", Files "+str(delfiles)+ \
-		". &emsp; ERRORS: "+str(errors)
-	fbartext = fbartext + "</p>"
-	return fbartext
+		fmtots.append((total_credirs, total_cpfiles, total_deldirs, total_delfiles, total_errors))
+		scrframe = fmlast
+	return render_template('logs.html', frames=frames, scrframe=scrframe, fmtots=fmtots, fmlast=fmlast)
 
 @app.route('/frame/<frameidstr>')
 def frame(frameidstr):
 	global scrframe
 
 	scrframe = int(frameidstr)
-	if scrframe < 0 or scrframe >= len(frames):
+	if scrframe < 0 or scrframe > fmlast:
 		logfile()
-	return pageheader() + framebar() + "<pre>" + frames[scrframe] + "</pre>" + "<hr>" + framebar()
+	return render_template('logs.html', frames=frames, scrframe=scrframe, fmtots=fmtots, fmlast=fmlast)
 
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", debug=False)
+	app.run(host="0.0.0.0", port=5000, debug=False)
--- sjwebui.sh
+++ sjwebui.sh
@@ -2,6 +2,5 @@
 
 export SJREGISTRY=/shares/USBDRIVE/SYNCJOBS.conf/etc/registry
 
-logfile=$(grep '^logfile' $SJREGISTRY | cut --delimiter=':' -f2)
 echo "STARTING =================="
 /home/pi/syncjobs/sjwebui.py
